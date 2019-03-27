c455f1996f434144ceb4798471b70a2b0c67f7a9
zhuoxiaojian <1933860854@qq.com>
2019-03-27 17:31:58 +0800

    第一版

--- src/main/java/com/nisure/BlogbyzhuoApplication.java
+++ src/main/java/com/nisure/BlogbyzhuoApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BlogbyzhuoApplication {
-
+	/**
+	 * 基于springboot2.1的系统
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(BlogbyzhuoApplication.class, args);
 	}
