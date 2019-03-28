6830b50e886e962513bca1abb110a4f4def0552e
zhuoxiaojian <1933860854@qq.com>
2019-03-28 10:33:02 +0800

    commit

--- src/main/java/com/nisure/repository/UserRepository.java
+++ src/main/java/com/nisure/repository/UserRepository.java
@@ -17,5 +17,6 @@ public interface UserRepository extends BaseRepository<User, Long> {
     @Query("select user from User user where user.name = ?1 and user.password = ?2")
     public User checkUser(String name, String password);
 
+    //属性方法 findBy + 属性
     public User findByNameAndPassword(String name, String password);
 }
